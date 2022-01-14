dd58f247fc0119ac3e9723a1e9e1312330fc3c08
finicu212 <smurfdeplastic@gmail.com>
2022-01-15 00:09:50 +0200

    stefan commits

---
+++ front_end/user.js
@@ -0,0 +1,16 @@
+const mainDiv = document.querySelector("#main")
+
+// GET all history:		http://localhost:3000/requests
+// GET all users:		http://localhost:3000/users
+// GET specific user:	http://localhost:3000/users/ip
+
+fetch("http://localhost:3000/requests")
+	.then(response => response.json())
+	.then(data => {
+		console.log(data)
+		data.forEach(request => { //get entry only if userip from request == userip of user
+			const title = '<div class="entry"><h3>' + request.websitePage + '</h3> - <p>' + request.websiteName + " - " + request.updatedAt + '</p></div>\n'; 
+			mainDiv.insertAdjacentHTML("beforeend", title);
+		})
+	})
+	.catch(err => console.log(err))
